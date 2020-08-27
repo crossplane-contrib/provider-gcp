8c6f2604bd6b91bd7ea0ded9c8d5a6ddc9b99544
Muvaffak Onus <onus.muvaffak@gmail.com>
2020-08-27 19:26:56 +0300

    add ProviderConfig type
    Signed-off-by: Muvaffak Onus <onus.muvaffak@gmail.com>

---
+++ apis/v1alpha3/providerconfig_types.go
@@ -0,0 +1,54 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package v1alpha3
+
+import (
+	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// A ProviderConfigSpec defines the desired state of a ProviderConfig.
+type ProviderConfigSpec struct {
+	runtimev1alpha1.ProviderConfigSpec `json:",inline"`
+
+	// ProjectID is the project name (not numerical ID) of this GCP ProviderConfig.
+	ProjectID string `json:"projectID"`
+}
+
+// +kubebuilder:object:root=true
+
+// A ProviderConfig configures a GCP 'provider', i.e. a connection to a particular
+// GCP project using a particular GCP service account
+// +kubebuilder:printcolumn:name="PROJECT-ID",type="string",JSONPath=".spec.projectID"
+// +kubebuilder:printcolumn:name="AGE",type="date",JSONPath=".metadata.creationTimestamp"
+// +kubebuilder:printcolumn:name="SECRET-NAME",type="string",JSONPath=".spec.credentialsSecretRef.name",priority=1
+// +kubebuilder:resource:scope=Cluster,categories={crossplane,provider,gcp}
+type ProviderConfig struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec ProviderConfigSpec `json:"spec"`
+}
+
+// +kubebuilder:object:root=true
+
+// ProviderConfigList contains a list of ProviderConfig
+type ProviderConfigList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ProviderConfig `json:"items"`
+}
