9248673967e64360c5e06ba30672bbd63dec9bdd
qiffang <947321353@qq.com>
2020-09-08 18:19:32 +0800

    Support: managed zone for gcp platform

---
+++ pkg/clients/dns/managedzone.go
@@ -0,0 +1,74 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dns
+
+import (
+	"fmt"
+
+	"google.golang.org/api/dns/v1"
+
+	"github.com/crossplane/provider-gcp/apis/dns/v1alpha1"
+)
+
+// GenerateManagedZone produces a ManagedZone that is configured via given ManagedZoneParameters.
+func GenerateManagedZone(projectID string, s v1alpha1.ManagedZoneParameters) *dns.ManagedZone {
+	network := &dns.ManagedZonePrivateVisibilityConfigNetwork{
+		Kind:       "dns#managedZonePrivateVisibilityConfigNetwork",
+		NetworkUrl: fmt.Sprintf("https://www.googleapis.com/compute/v1/projects/%s/global/networks/%s", projectID, s.Network),
+	}
+
+	visibility := "private"
+	if s.Visibility != "" {
+		visibility = s.Visibility
+	}
+
+	zone := &dns.ManagedZone{
+		Name:        s.Name,
+		DnsName:     s.DNSName,
+		Description: s.Description,
+		Visibility:  visibility,
+		PrivateVisibilityConfig: &dns.ManagedZonePrivateVisibilityConfig{
+			Kind:     "dns#managedZonePrivateVisibilityConfig",
+			Networks: []*dns.ManagedZonePrivateVisibilityConfigNetwork{network},
+		},
+	}
+
+	if s.Labels != nil && len(s.Labels) != 0 {
+		zone.Labels = s.Labels
+	}
+
+	return zone
+}
+
+// LateInitialize fills the empty fields in *v1alpha1.ManagedZoneParameters with
+// the values seen in ManagedZone.
+func LateInitialize(spec *v1alpha1.ManagedZoneParameters, obs *dns.ManagedZone) {
+	if obs == nil {
+		return
+	}
+
+	if len(spec.Labels) == 0 && len(obs.Labels) != 0 {
+		for k, v := range obs.Labels {
+			spec.Labels[k] = v
+		}
+	}
+}
+
+// IsUpToDate check whether the dns in Spec and Response are same or not
+func IsUpToDate(spec v1alpha1.ManagedZoneParameters, obs dns.ManagedZone) bool {
+	return spec.DNSName == obs.DnsName
+}
---
+++ pkg/controller/dns/managedzone.go
@@ -0,0 +1,188 @@
+/*
+Copyright 2020 The Crossplane Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package dns
+
+import (
+	"context"
+	"fmt"
+
+	"google.golang.org/api/option"
+	"k8s.io/apimachinery/pkg/types"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/pkg/errors"
+	"google.golang.org/api/dns/v1"
+	corev1 "k8s.io/api/core/v1"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	runtimev1alpha1 "github.com/crossplane/crossplane-runtime/apis/core/v1alpha1"
+	"github.com/crossplane/crossplane-runtime/pkg/event"
+	"github.com/crossplane/crossplane-runtime/pkg/logging"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
+	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+
+	"github.com/crossplane/provider-gcp/apis/dns/v1alpha1"
+	gcpv1alpha3 "github.com/crossplane/provider-gcp/apis/v1alpha3"
+	gcp "github.com/crossplane/provider-gcp/pkg/clients"
+	dns2 "github.com/crossplane/provider-gcp/pkg/clients/dns"
+)
+
+const (
+	errGetProvider       = "cannot get Provider"
+	errProviderSecretRef = "cannot find Secret reference on Provider"
+	errGetProviderSecret = "cannot get Provider Secret"
+
+	errNotManagedZone    = "managed resource is not of type Topic"
+	errNewClient         = "cannot create client"
+	errCreateManagedZone = "cannot create ManagedZone"
+	errGet               = "failed to get the ManagedZone resource"
+	errDelete            = "failed to delete the ManagedZone resource"
+	errUnexpectedObject  = "The managed resource is not an ManagedZone resource"
+	errKubeUpdate        = "failed to update the ManagedZone custom resource"
+)
+
+// SetupManagedZone adds a controller that reconciles ManagedZones.
+func SetupManagedZone(mgr ctrl.Manager, l logging.Logger) error {
+	name := managed.ControllerName(v1alpha1.ManagedZoneGroupKind)
+
+	return ctrl.NewControllerManagedBy(mgr).
+		Named(name).
+		For(&v1alpha1.ManagedZone{}).
+		Complete(managed.NewReconciler(mgr,
+			resource.ManagedKind(v1alpha1.ManagedZoneGroupVersionKind),
+			managed.WithExternalConnecter(&connecter{kube: mgr.GetClient()}),
+			managed.WithLogger(l.WithValues("controller", name)),
+			managed.WithInitializers(managed.NewNameAsExternalName(mgr.GetClient())),
+			managed.WithRecorder(event.NewAPIRecorder(mgr.GetEventRecorderFor(name)))))
+}
+
+type connecter struct {
+	kube client.Client
+}
+
+// Connect sets up iam client using credentials from the provider
+func (c *connecter) Connect(ctx context.Context, mg resource.Managed) (managed.ExternalClient, error) {
+	cr, ok := mg.(*v1alpha1.ManagedZone)
+	if !ok {
+		return nil, errors.New(errNotManagedZone)
+	}
+
+	p := &gcpv1alpha3.Provider{}
+	if err := c.kube.Get(ctx, types.NamespacedName{Name: cr.Spec.ProviderReference.Name}, p); err != nil {
+		return nil, errors.Wrap(err, errGetProvider)
+	}
+
+	if p.GetCredentialsSecretReference() == nil {
+		return nil, errors.New(errProviderSecretRef)
+	}
+
+	s := &corev1.Secret{}
+	n := types.NamespacedName{Namespace: p.Spec.CredentialsSecretRef.Namespace, Name: p.Spec.CredentialsSecretRef.Name}
+	if err := c.kube.Get(ctx, n, s); err != nil {
+		return nil, errors.Wrap(err, errGetProviderSecret)
+	}
+
+	dnsService, err := dns.NewService(ctx, option.WithCredentialsJSON(s.Data[p.Spec.CredentialsSecretRef.Key]))
+	if err != nil {
+		return nil, errors.Wrap(err, errNewClient)
+	}
+
+	return &managedZoneExternal{Service: dnsService, kube: c.kube, projectID: p.Spec.ProjectID}, nil
+}
+
+type managedZoneExternal struct {
+	kube client.Client
+	*dns.Service
+	projectID string
+}
+
+func (e *managedZoneExternal) Observe(ctx context.Context, mg resource.Managed) (managed.ExternalObservation, error) {
+	cr, ok := mg.(*v1alpha1.ManagedZone)
+	if !ok {
+		return managed.ExternalObservation{}, errors.New(errNotManagedZone)
+	}
+
+	if meta.GetExternalName(cr) == "" {
+		return managed.ExternalObservation{
+			ResourceExists: false,
+		}, nil
+	}
+
+	t, err := e.ManagedZones.Get(e.projectID, meta.GetExternalName(cr)).Do()
+	if err != nil {
+		return managed.ExternalObservation{}, errors.Wrap(resource.Ignore(gcp.IsErrorNotFound, err), errGet)
+	}
+
+	current := cr.Spec.ForProvider.DeepCopy()
+	dns2.LateInitialize(&cr.Spec.ForProvider, t)
+	if !cmp.Equal(current, &cr.Spec.ForProvider) {
+		if err := e.kube.Update(ctx, cr); err != nil {
+			return managed.ExternalObservation{}, errors.Wrap(err, errKubeUpdate)
+		}
+	}
+
+	cr.SetConditions(runtimev1alpha1.Available())
+	return managed.ExternalObservation{
+		ResourceExists:   true,
+		ResourceUpToDate: dns2.IsUpToDate(cr.Spec.ForProvider, *t),
+	}, nil
+
+}
+
+func (e *managedZoneExternal) Create(ctx context.Context, mg resource.Managed) (managed.ExternalCreation, error) {
+	cr, ok := mg.(*v1alpha1.ManagedZone)
+	if !ok {
+		return managed.ExternalCreation{}, errors.New(errNotManagedZone)
+	}
+	cr.SetConditions(runtimev1alpha1.Creating())
+
+	resp, err := e.ManagedZones.Create(e.projectID, dns2.GenerateManagedZone(e.projectID, cr.Spec.ForProvider)).Do()
+	if err != nil {
+		return managed.ExternalCreation{}, errors.Wrap(err, errCreateManagedZone)
+	}
+
+	meta.SetExternalName(cr, fmt.Sprintf("%d", resp.Id))
+	return managed.ExternalCreation{}, errors.Wrap(e.kube.Update(ctx, cr), errKubeUpdate)
+}
+
+func (e *managedZoneExternal) Update(ctx context.Context, mg resource.Managed) (managed.ExternalUpdate, error) {
+	return managed.ExternalUpdate{}, nil
+}
+
+func (e *managedZoneExternal) Delete(ctx context.Context, mg resource.Managed) error {
+	cr, ok := mg.(*v1alpha1.ManagedZone)
+	if !ok {
+		return errors.New(errUnexpectedObject)
+	}
+
+	cr.Status.SetConditions(runtimev1alpha1.Deleting())
+
+	phzID := meta.GetExternalName(cr)
+	resp, err := e.ResourceRecordSets.List(e.projectID, phzID).Do()
+	if err == nil {
+		for _, rs := range resp.Rrsets {
+			_, _ = e.Changes.Create(e.projectID, phzID, &dns.Change{
+				Deletions: []*dns.ResourceRecordSet{rs},
+			}).Context(ctx).Do()
+		}
+	}
+
+	err = e.ManagedZones.Delete(e.projectID, phzID).Do()
+	return errors.Wrap(resource.Ignore(gcp.IsErrorNotFound, err), errDelete)
+}
