9042e80be962602580d131110a5e753cf755db16
hasheddan <georgedanielmangum@gmail.com>
2021-02-04 15:58:34 -0600

    Explicitly drop InjectedIdentity as credential source
    Removes InjectedIdentity which has been a valid value for credentials
    source, but was not actually functional in practice.
    
    Signed-off-by: hasheddan <georgedanielmangum@gmail.com>

--- apis/v1beta1/providerconfig_types.go
+++ apis/v1beta1/providerconfig_types.go
@@ -34,7 +34,7 @@ type ProviderConfigSpec struct {
 // ProviderCredentials required to authenticate.
 type ProviderCredentials struct {
 	// Source of the provider credentials.
-	// +kubebuilder:validation:Enum=None;Secret;InjectedIdentity;Environment;Filesystem
+	// +kubebuilder:validation:Enum=None;Secret;Environment;Filesystem
 	Source xpv1.CredentialsSource `json:"source"`
 
 	xpv1.CommonCredentialSelectors `json:",inline"`
