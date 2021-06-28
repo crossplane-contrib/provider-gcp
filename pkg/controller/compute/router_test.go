8121591ee29054c8bbef7eab3f89ea4851e6ae2c
Pratyush Singhal <psinghal20@gmail.com>
2021-06-28 19:29:10 +0530

    chore(router/controller): add tests for controller for router managed resource
    Signed-off-by: Pratyush Singhal <psinghal20@gmail.com>

---
+++ pkg/controller/compute/router_test.go
@@ -0,0 +1,528 @@
+/*
+Copyright 2021 The Crossplane Authors.
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package compute
+
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/go-cmp/cmp"
+	"github.com/pkg/errors"
+	compute "google.golang.org/api/compute/v1"
+	"google.golang.org/api/option"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+
+	xpv1 "github.com/crossplane/crossplane-runtime/apis/common/v1"
+	"github.com/crossplane/crossplane-runtime/pkg/meta"
+	"github.com/crossplane/crossplane-runtime/pkg/reconciler/managed"
+	"github.com/crossplane/crossplane-runtime/pkg/resource"
+	"github.com/crossplane/crossplane-runtime/pkg/test"
+
+	"github.com/crossplane/provider-gcp/apis/compute/v1beta1"
+	"github.com/crossplane/provider-gcp/pkg/clients/router"
+)
+
+var _ managed.ExternalConnecter = &routerConnector{}
+var _ managed.ExternalClient = &routerExternal{}
+
+const (
+	testRouterName = "test-router"
+)
+
+type routerModifier func(*v1beta1.Router)
+
+func routerWithConditions(c ...xpv1.Condition) routerModifier {
+	return func(i *v1beta1.Router) { i.Status.SetConditions(c...) }
+}
+
+func routerWithDescription(d string) routerModifier {
+	return func(i *v1beta1.Router) { i.Spec.ForProvider.Description = &d }
+}
+
+func routerObj(im ...routerModifier) *v1beta1.Router {
+	i := &v1beta1.Router{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:       testRouterName,
+			Finalizers: []string{},
+			Annotations: map[string]string{
+				meta.AnnotationKeyExternalName: testRouterName,
+			},
+		},
+		Spec: v1beta1.RouterSpec{
+			ForProvider: v1beta1.RouterParameters{},
+		},
+	}
+
+	for _, m := range im {
+		m(i)
+	}
+
+	return i
+}
+
+func TestRouterObserve(t *testing.T) {
+	type args struct {
+		mg resource.Managed
+	}
+	type want struct {
+		mg  resource.Managed
+		obs managed.ExternalObservation
+		err error
+	}
+
+	cases := map[string]struct {
+		handler http.Handler
+		kube    client.Client
+		args    args
+		want    want
+	}{
+		"NotRouter": {
+			handler: nil,
+			args: args{
+				mg: &v1beta1.Subnetwork{},
+			},
+			want: want{
+				mg:  &v1beta1.Subnetwork{},
+				err: errors.New(errNotRouter),
+			},
+		},
+		"NotFound": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodGet, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusNotFound)
+				_ = json.NewEncoder(w).Encode(&compute.Address{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(),
+				err: nil,
+			},
+		},
+		"GetFailed": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodGet, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusBadRequest)
+				_ = json.NewEncoder(w).Encode(&compute.Address{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(),
+				err: errors.Wrap(gError(http.StatusBadRequest, ""), errGetRouter),
+			},
+		},
+		"NotUpToDateSpecUpdateFailed": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodGet, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusOK)
+				c := routerObj()
+				gn := &compute.Router{}
+				router.GenerateRouter(testRouterName, c.Spec.ForProvider, gn)
+				gn.Description = "a very interesting description"
+				_ = json.NewEncoder(w).Encode(gn)
+			}),
+			kube: &test.MockClient{
+				MockUpdate: test.NewMockUpdateFn(errBoom),
+			},
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithDescription("a very interesting description")),
+				err: errors.Wrap(errBoom, errManagedRouterUpdate),
+			},
+		},
+		"RunnableUnbound": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodGet, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusOK)
+				c := &compute.Router{}
+				router.GenerateRouter(testRouterName, routerObj().Spec.ForProvider, c)
+				_ = json.NewEncoder(w).Encode(c)
+			}),
+			kube: &test.MockClient{
+				MockGet: test.NewMockGetFn(nil),
+			},
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				obs: managed.ExternalObservation{
+					ResourceExists:   true,
+					ResourceUpToDate: true,
+				},
+				mg: routerObj(routerWithConditions(xpv1.Available())),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			server := httptest.NewServer(tc.handler)
+			defer server.Close()
+			s, _ := compute.NewService(context.Background(), option.WithEndpoint(server.URL), option.WithoutAuthentication())
+			e := routerExternal{
+				kube:      tc.kube,
+				projectID: projectID,
+				Service:   s,
+			}
+			obs, err := e.Observe(context.Background(), tc.args.mg)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("Observe(...): -want error, +got error:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.obs, obs); diff != "" {
+				t.Errorf("Observe(...): -want, +got:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.mg, tc.args.mg); diff != "" {
+				t.Errorf("Observe(...): -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestRouterCreate(t *testing.T) {
+	type args struct {
+		ctx context.Context
+		mg  resource.Managed
+	}
+	type want struct {
+		mg  resource.Managed
+		cre managed.ExternalCreation
+		err error
+	}
+
+	cases := map[string]struct {
+		handler http.Handler
+		kube    client.Client
+		args    args
+		want    want
+	}{
+		"NotRouter": {
+			handler: nil,
+			args: args{
+				mg: &v1beta1.Subnetwork{},
+			},
+			want: want{
+				mg:  &v1beta1.Subnetwork{},
+				err: errors.New(errNotRouter),
+			},
+		},
+		"Successful": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				if diff := cmp.Diff(http.MethodPost, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				i := &compute.Router{}
+				b, err := ioutil.ReadAll(r.Body)
+				if diff := cmp.Diff(err, nil); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				err = json.Unmarshal(b, i)
+				if diff := cmp.Diff(err, nil); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusOK)
+				_ = r.Body.Close()
+				_ = json.NewEncoder(w).Encode(&compute.Operation{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithConditions(xpv1.Creating())),
+				cre: managed.ExternalCreation{},
+				err: nil,
+			},
+		},
+		"AlreadyExists": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodPost, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusConflict)
+				_ = json.NewEncoder(w).Encode(&compute.Operation{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithConditions(xpv1.Creating())),
+				err: errors.Wrap(gError(http.StatusConflict, ""), errRouterCreateFailed),
+			},
+		},
+		"Failed": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodPost, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusBadRequest)
+				_ = json.NewEncoder(w).Encode(&compute.Operation{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithConditions(xpv1.Creating())),
+				err: errors.Wrap(gError(http.StatusBadRequest, ""), errRouterCreateFailed),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			server := httptest.NewServer(tc.handler)
+			defer server.Close()
+			s, _ := compute.NewService(context.Background(), option.WithEndpoint(server.URL), option.WithoutAuthentication())
+			e := routerExternal{
+				kube:      tc.kube,
+				projectID: projectID,
+				Service:   s,
+			}
+			_, err := e.Create(tc.args.ctx, tc.args.mg)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("Create(...): -want error, +got error:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.mg, tc.args.mg); diff != "" {
+				t.Errorf("Create(...): -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestRouterDelete(t *testing.T) {
+	type args struct {
+		mg resource.Managed
+	}
+	type want struct {
+		mg  resource.Managed
+		err error
+	}
+
+	cases := map[string]struct {
+		handler http.Handler
+		kube    client.Client
+		args    args
+		want    want
+	}{
+		"NotRouter": {
+			handler: nil,
+			args: args{
+				mg: &v1beta1.Subnetwork{},
+			},
+			want: want{
+				mg:  &v1beta1.Subnetwork{},
+				err: errors.New(errNotRouter),
+			},
+		},
+		"Successful": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodDelete, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusOK)
+				_ = json.NewEncoder(w).Encode(&compute.Operation{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithConditions(xpv1.Deleting())),
+				err: nil,
+			},
+		},
+		"AlreadyGone": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodDelete, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusNotFound)
+				_ = json.NewEncoder(w).Encode(&compute.Operation{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithConditions(xpv1.Deleting())),
+				err: nil,
+			},
+		},
+		"Failed": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				if diff := cmp.Diff(http.MethodDelete, r.Method); diff != "" {
+					t.Errorf("r: -want, +got:\n%s", diff)
+				}
+				w.WriteHeader(http.StatusBadRequest)
+				_ = json.NewEncoder(w).Encode(&compute.Operation{})
+			}),
+			args: args{
+				mg: routerObj(),
+			},
+			want: want{
+				mg:  routerObj(routerWithConditions(xpv1.Deleting())),
+				err: errors.Wrap(gError(http.StatusBadRequest, ""), errRouterDeleteFailed),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			server := httptest.NewServer(tc.handler)
+			defer server.Close()
+			s, _ := compute.NewService(context.Background(), option.WithEndpoint(server.URL), option.WithoutAuthentication())
+			e := routerExternal{
+				kube:      tc.kube,
+				projectID: projectID,
+				Service:   s,
+			}
+			err := e.Delete(context.Background(), tc.args.mg)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("Delete(...): -want error, +got error:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.mg, tc.args.mg); diff != "" {
+				t.Errorf("Delete(...): -want, +got:\n%s", diff)
+			}
+		})
+	}
+}
+
+func TestRouterUpdate(t *testing.T) {
+	type args struct {
+		mg resource.Managed
+	}
+	type want struct {
+		mg  resource.Managed
+		upd managed.ExternalUpdate
+		err error
+	}
+
+	cases := map[string]struct {
+		handler http.Handler
+		kube    client.Client
+		args    args
+		want    want
+	}{
+		"NotRouter": {
+			handler: nil,
+			args: args{
+				mg: &v1beta1.Subnetwork{},
+			},
+			want: want{
+				mg:  &v1beta1.Subnetwork{},
+				err: errors.New(errNotRouter),
+			},
+		},
+		"Successful": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				switch r.Method {
+				case http.MethodGet:
+					w.WriteHeader(http.StatusOK)
+					_ = json.NewEncoder(w).Encode(&compute.Network{})
+				case http.MethodPatch:
+					w.WriteHeader(http.StatusOK)
+					_ = json.NewEncoder(w).Encode(&compute.Operation{})
+				default:
+					w.WriteHeader(http.StatusBadRequest)
+					_ = json.NewEncoder(w).Encode(&compute.Operation{})
+				}
+			}),
+			kube: &test.MockClient{
+				MockGet: test.NewMockGetFn(nil),
+			},
+			args: args{
+				mg: routerObj(routerWithDescription("a new description")),
+			},
+			want: want{
+				mg:  routerObj(routerWithDescription("a new description")),
+				err: nil,
+			},
+		},
+		"UpdateFails": {
+			handler: http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+				_ = r.Body.Close()
+				switch r.Method {
+				case http.MethodGet:
+					w.WriteHeader(http.StatusOK)
+					_ = json.NewEncoder(w).Encode(&compute.Firewall{})
+				case http.MethodPatch:
+					w.WriteHeader(http.StatusBadRequest)
+					_ = json.NewEncoder(w).Encode(&compute.Operation{})
+				default:
+					w.WriteHeader(http.StatusOK)
+					_ = json.NewEncoder(w).Encode(&compute.Operation{})
+				}
+			}),
+			kube: &test.MockClient{
+				MockGet: test.NewMockGetFn(nil),
+			},
+			args: args{
+				// Must include field that causes update.
+				mg: routerObj(routerWithDescription("a new description")),
+			},
+			want: want{
+				mg:  routerObj(routerWithDescription("a new description")),
+				err: errors.Wrap(gError(http.StatusBadRequest, ""), errRouterUpdateFailed),
+			},
+		},
+	}
+
+	for name, tc := range cases {
+		t.Run(name, func(t *testing.T) {
+			server := httptest.NewServer(tc.handler)
+			defer server.Close()
+			s, _ := compute.NewService(context.Background(), option.WithEndpoint(server.URL), option.WithoutAuthentication())
+			e := routerExternal{
+				kube:      tc.kube,
+				projectID: projectID,
+				Service:   s,
+			}
+			upd, err := e.Update(context.Background(), tc.args.mg)
+			if diff := cmp.Diff(tc.want.err, err, test.EquateErrors()); diff != "" {
+				t.Errorf("Update(...): -want error, +got error:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.mg, tc.args.mg); diff != "" {
+				t.Errorf("Update(...): -want, +got:\n%s", diff)
+			}
+			if diff := cmp.Diff(tc.want.upd, upd); diff != "" {
+				t.Errorf("Update(...): -want, +got:\n%s", diff)
+			}
+
+		})
+	}
+}
