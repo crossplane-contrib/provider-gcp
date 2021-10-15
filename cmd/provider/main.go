b6219ddf7ed045cd6726855a458c3de92eb4cc07
Nic Cope <negz@rk0n.org>
2021-10-15 22:10:04 +0000

    Switch to lease based leader election
    The default in controller-runtime is to use the 'configmapsleases' strategy,
    which takes a lock using both a ConfigMap and a Lease. This is done for backward
    compatibility. For reasons that are not immediately clear to me (presumably some
    kind of saturation) we find that at --max-reconcile-rate=100 we start to see
    leadership losses because we can't renew our lease before the 10 second renewal
    deadline is up. This should be a safe change since we've been using
    configmapleases for many releases, so I don't expect dropping configmaps to
    affect anyone. (We also don't create more than one replica by default.)
    
    Signed-off-by: Nic Cope <negz@rk0n.org>

--- cmd/provider/main.go
+++ cmd/provider/main.go
@@ -19,8 +19,10 @@ package main
 import (
 	"os"
 	"path/filepath"
+	"time"
 
 	"gopkg.in/alecthomas/kingpin.v2"
+	"k8s.io/client-go/tools/leaderelection/resourcelock"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 
@@ -57,9 +59,20 @@ func main() {
 	kingpin.FatalIfError(err, "Cannot get API server rest config")
 
 	mgr, err := ctrl.NewManager(ratelimiter.LimitRESTConfig(cfg, *maxReconcileRate), ctrl.Options{
-		LeaderElection:   *leaderElection,
-		LeaderElectionID: "crossplane-leader-election-provider-gcp",
-		SyncPeriod:       syncInterval,
+		SyncPeriod: syncInterval,
+
+		// controller-runtime uses both ConfigMaps and Leases for leader
+		// election by default. Leases expire after 15 seconds, with a
+		// 10 second renewal deadline. We've observed leader loss due to
+		// renewal deadlines being exceeded when under high load - i.e.
+		// hundreds of reconciles per second and ~200rps to the API
+		// server. Switching to Leases only and longer leases appears to
+		// alleviate this.
+		LeaderElection:             *leaderElection,
+		LeaderElectionID:           "crossplane-leader-election-provider-gcp",
+		LeaderElectionResourceLock: resourcelock.LeasesResourceLock,
+		LeaseDuration:              func() *time.Duration { d := 60 * time.Second; return &d }(),
+		RenewDeadline:              func() *time.Duration { d := 50 * time.Second; return &d }(),
 	})
 	kingpin.FatalIfError(err, "Cannot create controller manager")
 
