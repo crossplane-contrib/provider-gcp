763bf3601dd513a7b3abc7ff0b0475be17e7d34a
Hasan Turken <turkenh@gmail.com>
2022-04-08 04:26:49 +0300

    Copy jet apis directories
    Signed-off-by: Hasan Turken <turkenh@gmail.com>

--- apis/generate.go
+++ apis/generate.go
@@ -23,8 +23,17 @@ limitations under the License.
 // Remove existing CRDs
 //go:generate rm -rf ../package/crds
 
+// Remove generated Go files
+//go:generate bash -c "find . -iname 'zz_*' -delete"
+//go:generate bash -c "find . -type d -empty -delete"
+//go:generate bash -c "find ../internal/controller -iname 'zz_*' -delete"
+//go:generate bash -c "find ../internal/controller -type d -empty -delete"
+
+// Run Terrajet generator
+//go:generate go run ../cmd/generator/main.go ..
+
 // Generate deepcopy methodsets and CRD manifests
-//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=./... crd:crdVersions=v1 output:artifacts:config=../package/crds
+//go:generate go run -tags generate sigs.k8s.io/controller-tools/cmd/controller-gen object:headerFile=../hack/boilerplate.go.txt paths=./... crd:allowDangerousTypes=true,crdVersions=v1 output:artifacts:config=../package/crds
 
 // Generate crossplane-runtime methodsets (resource.Managed, etc)
 //go:generate go run -tags generate github.com/crossplane/crossplane-tools/cmd/angryjet generate-methodsets --header-file=../hack/boilerplate.go.txt ./...
