13b091239994b67ece15d3c44044c0ce4e0a5f90
Ruhika <ruhika406@gmail.com>
2022-08-11 12:25:01 +0530

    testBreakingChanges: recursive traversing to print keys that do not exist
    Signed-off-by: Ruhika <ruhika406@gmail.com>

--- cmd/testBreakingChanges/main.go
+++ cmd/testBreakingChanges/main.go
@@ -16,8 +16,62 @@ limitations under the License.
 
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	v1 "k8s.io/apiextensions-apiserver/pkg/apis/apiextensions/v1"
+	"k8s.io/apimachinery/pkg/util/yaml"
+)
 
 func main() {
 	fmt.Println("----Target successful,this file inside cmd/testBreakingChanges runs successfully----")
+
+	oldfile, _ := os.ReadFile("old.yaml")
+	newfile, _ := os.ReadFile("new.yaml")
+
+	old := &v1.CustomResourceDefinition{}
+	err := yaml.Unmarshal(oldfile, old)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	new := &v1.CustomResourceDefinition{}
+	err2 := yaml.Unmarshal(newfile, new)
+	if err != nil {
+		fmt.Println(err2)
+	}
+
+	PrintFields(old.Spec.Versions[0].Schema.OpenAPIV3Schema, "", new.Spec.Versions[0].Schema.OpenAPIV3Schema)
+
+}
+
+func PrintFields(sch *v1.JSONSchemaProps, prefix string, newSchema *v1.JSONSchemaProps) {
+	if len(sch.Properties) == 0 {
+		return
+	}
+
+	for key, val := range sch.Properties {
+
+		var temp string
+
+		if prefix == "" {
+			temp = key
+		} else {
+			temp = prefix + "." + key
+		}
+
+		prop, ok := newSchema.Properties[key]
+		if !ok {
+			fmt.Printf("%s: does not exist.\n", temp)
+
+		}
+		// to print every other key which exists:
+		// else {
+		// 	fmt.Printf("%s\n", temp)
+		// }
+
+		PrintFields(&val, temp, &prop)
+
+	}
 }
